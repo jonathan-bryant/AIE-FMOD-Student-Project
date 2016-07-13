f5f1693d1768d94d7b72842694c71148ae4119c5
Cameron Baron <twitchin86@gmail.com>
2016-07-13 14:39:11 +1000

    Added player movement (basic) and sound player in middle of room

---
+++ AIE-Fmod/Assets/Scripts/Sound VFX/Movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.position += transform.forward * 0.1f;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position -= transform.forward * 0.1f;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.Rotate(new Vector3(0, 1, 0), -5);
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.Rotate(new Vector3(0, 1, 0), 5);
+		}
+	}
+}
---
+++ AIE-Fmod/Assets/Scripts/Sound VFX/SoundVFXPlayer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundVFXPlayer : MonoBehaviour
+{
+	[FMODUnity.EventRef]
+	public string m_musicRef;
+	private FMOD.Studio.EventInstance m_musicEvent;   // Creating an event will allow us to manage the sound over its lifetime.
+	private FMOD.Studio.EventDescription m_musicDesc;
+
+	private string m_levelString = "Level";
+	private FMOD.Studio.ParameterInstance m_level;
+	[Range(0,80)]	public float m_levelValue;
+
+	private FMODUnity.ParamRef[] m_params;
+
+	void Start ()
+	{
+		m_musicEvent = FMODUnity.RuntimeManager.CreateInstance(m_musicRef);
+		m_musicEvent.getParameter(m_levelString, out m_level);
+		m_musicEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(this.gameObject));
+		FMODUnity.RuntimeManager.AttachInstanceToGameObject(m_musicEvent, gameObject.transform, null);
+
+		m_musicEvent.start();
+	}
+
+	void OnDestroy()
+	{
+		m_musicEvent.release();
+	}
+	
+	void Update ()
+	{
+		if (m_musicEvent != null)
+		{
+			FMOD.Studio.PLAYBACK_STATE playBackState;
+			m_musicEvent.getPlaybackState(out playBackState);
+			if (playBackState == FMOD.Studio.PLAYBACK_STATE.STOPPED)
+			{
+				m_musicEvent.release();
+				m_musicEvent = null;
+				SendMessage("Music Stopped!");
+			}
+		}
+
+		m_musicEvent.setParameterValue(m_levelString, m_levelValue);
+	}
+	
+	void StopAllSoundEvents()
+	{
+		FMOD.Studio.Bus masterBus = FMODUnity.RuntimeManager.GetBus("bus:/"); // Gets master bus.
+		masterBus.stopAllEvents(FMOD.Studio.STOP_MODE.IMMEDIATE);
+	}
+}
