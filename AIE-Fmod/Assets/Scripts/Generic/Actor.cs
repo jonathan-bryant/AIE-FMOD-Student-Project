2011f2aaaa07e97398ea699fd2ffc59b9a610ed0
Matthew Zelenko Fmod <matthew@zelenko.com.au>
2016-07-13 14:55:35 +1000

    Added actor class and surfaces class. Scripts should go into folders corresponding to their appropriate scene name for clarity towards the user. Surface room: -Can walk onto different surfaces and it will play different sounds depending on type of surface

---
+++ AIE-Fmod/Assets/Scripts/Generic/Actor.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actor : MonoBehaviour
+{
+
+    [FMODUnity.EventRef]
+    public string m_footstepSurfaceName;
+    protected FMOD.Studio.EventInstance m_footstepSurfaceEvent;
+    protected FMOD.Studio.ParameterInstance m_footstepSurfaceParamter;
+
+    public float m_movementSpeed;
+    public float m_LookSensitivity;
+    Rigidbody m_rb;
+    Camera m_playerCamera;
+    bool m_IsWalking = true;
+
+   
+
+    public void Start()
+    {
+        m_rb = GetComponent<Rigidbody>();
+
+        //FMOD: Create insance of footsteps event
+        m_footstepSurfaceEvent = FMODUnity.RuntimeManager.CreateInstance(m_footstepSurfaceName);
+        //FMOD: Get a reference to the surface paramater and store it in a ParamaterInstance
+        m_footstepSurfaceEvent.getParameter("Surface", out m_footstepSurfaceParamter);
+        m_IsWalking = false;
+    
+        m_playerCamera = Camera.main;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    
+    public void Update()
+    {
+        transform.rotation = transform.rotation * Quaternion.Euler(0.0f, Input.GetAxis("Mouse X") * m_LookSensitivity, 0.0f);
+        m_playerCamera.transform.rotation = m_playerCamera.transform.rotation * Quaternion.Euler(-Input.GetAxis("Mouse Y") * m_LookSensitivity, 0.0f, 0.0f);
+
+        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+        {
+            if (m_IsWalking)
+            {
+                m_footstepSurfaceEvent.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+                m_IsWalking = false;
+            }
+        }
+        else
+        {
+            if (!m_IsWalking)
+            {
+                m_footstepSurfaceEvent.start();
+                m_IsWalking = true;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                m_rb.position += -transform.right * m_movementSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                m_rb.position += transform.right * m_movementSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                m_rb.position += transform.forward * m_movementSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                m_rb.position += -transform.forward * m_movementSpeed * Time.deltaTime;
+            }
+        }
+        m_footstepSurfaceEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject.transform, m_rb));
+    }
+
+    public void FixedUpdate()
+    {
+        RaycastHit info;
+        if(Physics.Raycast(transform.position, new Vector3(0,-1,0), out info, 10.0f))
+        {
+            if(info.collider.gameObject.tag == "Ground")
+            {
+                transform.position = new Vector3(transform.position.x, info.point.y + transform.localScale.y, transform.position.z);
+            }
+        }
+    }
+}
---
+++ AIE-Fmod/Assets/Scripts/Surface Room/Surfaces_Actor.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Surfaces_Actor : Actor {
+
+
+
+    // Use this for initialization
+    new void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    new void Update()
+    {
+        base.Update();
+    }
+
+    new void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+
+    void OnTriggerEnter(Collider a_collider)
+    {
+        string tag = a_collider.gameObject.tag;
+        if (tag == "Grass")
+        {
+            m_footstepSurfaceParamter.setValue(2.0f);
+            Debug.Log("Grass footsteps");
+        }
+        else if (tag == "Carpet")
+        {
+            m_footstepSurfaceParamter.setValue(1.0f);
+            Debug.Log("Carpet footsteps");
+        }
+        else if (tag == "Wood")
+        {
+            m_footstepSurfaceParamter.setValue(3.0f);
+            Debug.Log("Wood footsteps");
+        }
+    }
+    void OnTriggerExit(Collider a_collider)
+    {
+        string tag = a_collider.gameObject.tag;
+        if (tag == "Grass" || tag == "Carpet" || tag == "Wood")
+        {
+            m_footstepSurfaceParamter.setValue(0.0f);
+        }
+    }
+}
