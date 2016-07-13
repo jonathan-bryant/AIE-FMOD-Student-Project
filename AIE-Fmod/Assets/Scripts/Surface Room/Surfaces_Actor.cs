06610d19bdba47761aa64ad4bd43d4771a45fddc
Matthew Zelenko Fmod <matthew@zelenko.com.au>
2016-07-13 16:12:40 +1000

    Added some comments.

--- AIE-Fmod/Assets/Scripts/Surface Room/Surfaces_Actor.cs
+++ AIE-Fmod/Assets/Scripts/Surface Room/Surfaces_Actor.cs
@@ -2,16 +2,12 @@
 using System.Collections;
 
 public class Surfaces_Actor : Actor {
-
-
-
-    // Use this for initialization
+    
     new void Start()
     {
         base.Start();
     }
-
-    // Update is called once per frame
+    
     new void Update()
     {
         base.Update();
@@ -24,25 +20,24 @@ public class Surfaces_Actor : Actor {
 
     void OnTriggerEnter(Collider a_collider)
     {
+        //Fmod: Depending on the type of ground that is triggered, set surface parameter to change the sound of the footstep.
         string tag = a_collider.gameObject.tag;
         if (tag == "Grass")
         {
             m_footstepSurfaceParamter.setValue(2.0f);
-            Debug.Log("Grass footsteps");
         }
         else if (tag == "Carpet")
         {
             m_footstepSurfaceParamter.setValue(1.0f);
-            Debug.Log("Carpet footsteps");
         }
         else if (tag == "Wood")
         {
             m_footstepSurfaceParamter.setValue(3.0f);
-            Debug.Log("Wood footsteps");
         }
     }
     void OnTriggerExit(Collider a_collider)
     {
+        //Fmod: When trigger exit happens on grass, carpet or wood, set surface parameter to 0(which is silent).
         string tag = a_collider.gameObject.tag;
         if (tag == "Grass" || tag == "Carpet" || tag == "Wood")
         {
