e06c2d4f976b5195da36eebc28a67470e83c4076
Matthew Zelenko <matthew@zelenko.com.au>
2016-07-15 16:31:00 +1000

    Started Wild West. Changed SurfacesActor to just Surfaces, does not inherit Actor, separate classes. Changed Actor to use CharacterController, not Rigidbody and Colliders.

--- AIE-Fmod/Assets/Scripts/Generic/Actor.cs
+++ AIE-Fmod/Assets/Scripts/Generic/Actor.cs
@@ -3,79 +3,59 @@ using System.Collections;
 
 public class Actor : MonoBehaviour
 {
-    //Fmod: Call this to display it in Unity Inspector.
-    [FMODUnity.EventRef]
-    //Fmod: Name of Event. Used in conjunction with EventInstance.
-    public string m_footstepSurfaceName;
-    //Fmod: EventInstance. Used to play or stop the sound, etc.
-    protected FMOD.Studio.EventInstance m_footstepSurfaceEvent;
-    //Fmod: Parameter. Used to adjust EventInstances tracks. Such as: changing from wood to a carpet floor inside the same Event.
-    protected FMOD.Studio.ParameterInstance m_footstepSurfaceParamter;
-    
+
+
     public float m_movementSpeed;
     public float m_LookSensitivity;
-    Rigidbody m_rb;
     Camera m_playerCamera;
-    bool m_IsWalking = true;
+    bool m_IsWalking;
+    public bool IsWalking { get { return this.m_IsWalking; } }
+    CharacterController m_cc;
+    Vector3 m_moveDirection;
 
     public void Start()
     {
-        m_rb = GetComponent<Rigidbody>();
-
-        //FMOD: Create insance of footsteps event.
-        m_footstepSurfaceEvent = FMODUnity.RuntimeManager.CreateInstance(m_footstepSurfaceName);
-        //FMOD: Get a reference to the surface paramater and store it in a ParamaterInstance.
-        m_footstepSurfaceEvent.getParameter("Surface", out m_footstepSurfaceParamter);
-        //Fmod: EventInstance.start() Safeguard. Used in Update().
         m_IsWalking = false;
 
         m_playerCamera = Camera.main;
         Cursor.lockState = CursorLockMode.Locked;
+        m_cc = GetComponent<CharacterController>();
+        m_moveDirection = Vector3.zero;
     }
 
     public void Update()
     {
+        m_moveDirection = Vector3.zero;
         transform.rotation = transform.rotation * Quaternion.Euler(0.0f, Input.GetAxis("Mouse X") * m_LookSensitivity, 0.0f);
         m_playerCamera.transform.rotation = m_playerCamera.transform.rotation * Quaternion.Euler(-Input.GetAxis("Mouse Y") * m_LookSensitivity, 0.0f, 0.0f);
 
         if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
         {
-            if (m_IsWalking)
-            {
-                //Fmod: When actor is idle, stop playing the sound.
-                m_footstepSurfaceEvent.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
-                m_IsWalking = false;
-            }
+            m_IsWalking = false;
         }
         else
         {
-            if (!m_IsWalking)
-            {
-                //Fmod: When actor is walking, start EventInstance. Calling EventInstance.start() will play the EventInstance from the beginning, so be sure to safeguard the call.
-                m_footstepSurfaceEvent.start();
-                //Fmod: Need to attach instance to gameobject, so the sound can follow the player. Need to call this again everytime you start the EventInstance.
-                FMODUnity.RuntimeManager.AttachInstanceToGameObject(m_footstepSurfaceEvent, transform, m_rb);
-                m_IsWalking = true;
-            }
+            m_IsWalking = true;
             if (Input.GetKey(KeyCode.A))
             {
-                transform.position += -transform.right * m_movementSpeed * Time.deltaTime;
+                m_moveDirection += -transform.right;
             }
             if (Input.GetKey(KeyCode.D))
             {
-                transform.position += transform.right * m_movementSpeed * Time.deltaTime;
+                m_moveDirection += transform.right;
             }
             if (Input.GetKey(KeyCode.W))
             {
-                transform.position += transform.forward * m_movementSpeed * Time.deltaTime;
+                m_moveDirection += transform.forward;
             }
             if (Input.GetKey(KeyCode.S))
             {
-                transform.position += -transform.forward * m_movementSpeed * Time.deltaTime;
+                m_moveDirection += -transform.forward;
             }
+            m_moveDirection *= m_movementSpeed;
         }
-        //Fmod: Have to set EventInstance position to current position every frame, otherwise the sound will come from where the actor started.
-        //m_footstepSurfaceEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject.transform, m_rb));
+        m_moveDirection.y -= 9.8f;
+        m_cc.Move(m_moveDirection * Time.deltaTime);
         if (Input.GetKeyDown(KeyCode.LeftAlt))
         {
             if (Cursor.lockState != CursorLockMode.Locked)
@@ -93,13 +73,5 @@ public class Actor : MonoBehaviour
 
     public void FixedUpdate()
     {
-        RaycastHit info;
-        if (Physics.Raycast(transform.position, new Vector3(0, -1, 0), out info, 10.0f))
-        {
-            if (info.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
-            {
-                transform.position = new Vector3(transform.position.x, info.point.y + transform.localScale.y, transform.position.z);
-            }
-        }
     }
 }
---
+++ AIE-Fmod/Assets/Scripts/Wild West/WildWest_Actor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WildWest_Actor : Actor
+{
+
+    new void Start()
+    {
+        base.Start();
+
+    }
+
+    new void Update()
+    {
+        base.Update();
+    }
+    new void FixedUpdate()
+    {
+
+        base.FixedUpdate();
+    }
+
+    void OnTriggerEnter(Collider a_collider)
+    {
+
+    }
+    void OnTriggerExit(Collider a_collider)
+    {
+
+    }
+}
