790ea6a05fdcc0a4ae6cf9c9b76dbdc51b9bdf6a
Matthew Zelenko Fmod <matthew@zelenko.com.au>
2016-07-18 17:30:12 +1000

    Messing around with tranceivers

--- AIE-Fmod/Assets/Scripts/Generic/Actor.cs
+++ AIE-Fmod/Assets/Scripts/Generic/Actor.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class Actor : MonoBehaviour
 {
-
-
     public float m_movementSpeed;
     public float m_currentSpeed;
     public float m_LookSensitivity;
@@ -13,9 +11,11 @@ public class Actor : MonoBehaviour
     public bool IsWalking { get { return this.m_IsWalking; } }
     CharacterController m_cc;
     Vector3 m_moveDirection;
+    bool m_disableMovement;
 
     public void Start()
     {
+        Application.runInBackground = true;
         m_IsWalking = false;
 
         m_playerCamera = Camera.main;
@@ -27,39 +27,43 @@ public class Actor : MonoBehaviour
 
     public void Update()
     {
-        m_moveDirection = Vector3.zero;
-        transform.rotation = transform.rotation * Quaternion.Euler(0.0f, Input.GetAxis("Mouse X") * m_LookSensitivity, 0.0f);
-        m_playerCamera.transform.rotation = m_playerCamera.transform.rotation * Quaternion.Euler(-Input.GetAxis("Mouse Y") * m_LookSensitivity, 0.0f, 0.0f);
+        if (!m_disableMovement)
+        {
+            m_moveDirection = Vector3.zero;
+            transform.rotation = transform.rotation * Quaternion.Euler(0.0f, Input.GetAxis("Mouse X") * m_LookSensitivity, 0.0f);
+            m_playerCamera.transform.rotation = m_playerCamera.transform.rotation * Quaternion.Euler(-Input.GetAxis("Mouse Y") * m_LookSensitivity, 0.0f, 0.0f);
 
-        if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
-        {
-            m_IsWalking = false;
+            if (!Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.D))
+            {
+                m_IsWalking = false;
+            }
+            else
+            {
+                m_IsWalking = true;
+                if (Input.GetKey(KeyCode.A))
+                {
+                    m_moveDirection += -transform.right;
+                }
+                if (Input.GetKey(KeyCode.D))
+                {
+                    m_moveDirection += transform.right;
+                }
+                if (Input.GetKey(KeyCode.W))
+                {
+                    m_moveDirection += transform.forward;
+                }
+                if (Input.GetKey(KeyCode.S))
+                {
+                    m_moveDirection += -transform.forward;
+                }
+                m_moveDirection *= m_currentSpeed;
+            }
+            m_moveDirection.y -= 9.8f;
+            m_cc.Move(m_moveDirection * Time.deltaTime);
         }
-        else
-        {
-            m_IsWalking = true;
-            if (Input.GetKey(KeyCode.A))
-            {
-                m_moveDirection += -transform.right;
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                m_moveDirection += transform.right;
-            }
-            if (Input.GetKey(KeyCode.W))
-            {
-                m_moveDirection += transform.forward;
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                m_moveDirection += -transform.forward;
-            }
-            m_moveDirection *= m_currentSpeed;
-        }
-        m_moveDirection.y -= 9.8f;
-        m_cc.Move(m_moveDirection * Time.deltaTime);
         if (Input.GetKeyDown(KeyCode.LeftAlt))
         {
+            m_disableMovement = !m_disableMovement;
             if (Cursor.lockState != CursorLockMode.Locked)
             {
                 Cursor.lockState = CursorLockMode.Locked;
