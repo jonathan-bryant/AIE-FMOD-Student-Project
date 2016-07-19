f87fd37b429a4bdf200469e640fdced28b1ca0b5
Cameron Baron <twitchin86@gmail.com>
2016-07-19 17:19:34 +1000

    Got occlusion working!!! with low level sounds

--- AIE-Fmod/Assets/Scripts/Sound VFX/Occlusion.cs
+++ AIE-Fmod/Assets/Scripts/Sound VFX/Occlusion.cs
@@ -14,27 +14,35 @@ public class Occlusion : MonoBehaviour
 
 	void Start ()
 	{
+		mesh = new Mesh();
 		mesh = GetComponent<MeshFilter>().mesh;
 		numVerts = mesh.vertexCount;
-
+		
 		geomVerts = new FMOD.VECTOR[6][];
-		FMODUnity.RuntimeManager.LowlevelSystem.createGeometry(numPolygons, numVerts, out geometry);
-
+		result = FMODUnity.RuntimeManager.LowlevelSystem.createGeometry(numPolygons, numVerts, out geometry);
+		
 		// Geometry polygons have to be on the same plane, or things will break
 		for (int i = 0; i < 6; ++i)
 		{
 			FMOD.VECTOR[] temp = new FMOD.VECTOR[4];
-			for (int j = 0; j < 4; ++j)
-			{
-				temp[j] = FMODUnity.RuntimeUtils.ToFMODVector(mesh.vertices[4 * i + j]);
-			}
-			geomVerts[i] = temp;
-		}
+			//for (int j = 0; j < 4; ++j)
+			//{
+			//	//temp[j] = FMODUnity.RuntimeUtils.ToFMODVector(mesh.vertices[4 * i + j]);
+			//}
 
-		for (int i = 0; i < 6; ++i)
-		{
-			result = geometry.addPolygon(0.2f, 0.0f, true, 4, geomVerts[i], out polygons[i]);
-			Debug.Log(result);
+			// Had to swap the 3rd and 4th verts so that the whole face of the prism, instead of in triangles
+			Vector3 scale = transform.localScale;
+			Debug.Log(scale);
+			
+			temp[0] = FMODUnity.RuntimeUtils.ToFMODVector(MultiplyByScale(mesh.vertices[4 * i + 0], scale));
+			temp[1] = FMODUnity.RuntimeUtils.ToFMODVector(MultiplyByScale(mesh.vertices[4 * i + 1], scale));
+			temp[2] = FMODUnity.RuntimeUtils.ToFMODVector(MultiplyByScale(mesh.vertices[4 * i + 3], scale));
+			temp[3] = FMODUnity.RuntimeUtils.ToFMODVector(MultiplyByScale(mesh.vertices[4 * i + 2], scale));
+
+
+			geomVerts[i] = temp;
+			
+			result = geometry.addPolygon(1.0f, 1.0f, true, 4, geomVerts[i], out polygons[i]);
 		}
 	}
 	
@@ -43,9 +51,23 @@ public class Occlusion : MonoBehaviour
 	{
 		FMOD.VECTOR tempPos = FMODUnity.RuntimeUtils.ToFMODVector(transform.position);
 		geometry.setPosition(ref tempPos);
-
+		
 		FMOD.VECTOR pos;
-		geometry.getPosition(out pos);
-		Debug.Log(pos.x);
+		result = geometry.getPosition(out pos);
+
+		FMOD.VECTOR vertexInfo;
+		for (int i = 0; i < 4; i++)
+		{
+			geometry.getPolygonVertex(0, i, out vertexInfo);
+		}
+	}
+
+	Vector3 MultiplyByScale(Vector3 a_vec, Vector3 a_scale)
+	{
+		Vector3 result = a_vec;
+		result.x *= a_scale.x;
+		result.y *= a_scale.y;
+		result.z *= a_scale.z;
+		return result;
 	}
 }
---
+++ AIE-Fmod/Assets/Scripts/Sound VFX/ProgrammerSound.cs
@@ -0,0 +1,57 @@
+﻿/*=================================================================
+Project:		AIE-Fmod
+Developer:		Cameron Baron
+Company:		FMOD
+Date:			18/07/16
+==================================================================*/
+
+using UnityEngine;
+
+
+public class ProgrammerSound : MonoBehaviour 
+{
+	// Public Vars
+	public string m_soundPath = "/Assets/SoundVFX/7nation.mp3";
+	public FMOD.Sound m_sound;
+	public FMOD.Channel m_channel;
+	public FMOD.ChannelGroup m_channelGroup;
+
+	// Private Vars
+	private FMOD.RESULT result;
+
+	#region Unity Functions
+
+	void Start () 
+	{
+		result = FMODUnity.RuntimeManager.LowlevelSystem.createChannelGroup("Music Group", out m_channelGroup);
+		result = FMODUnity.RuntimeManager.LowlevelSystem.createSound(m_soundPath, FMOD.MODE.CREATESTREAM | FMOD.MODE._3D, out m_sound);
+		
+		Debug.Log("Create Sound: " + result);
+
+		if (result == FMOD.RESULT.OK)
+		{
+			PlaySound();
+		}
+	}
+	
+	void Update () 
+	{
+		
+	}
+
+	#endregion
+
+	public bool PlaySound()
+	{
+		result = FMODUnity.RuntimeManager.LowlevelSystem.playSound(m_sound, m_channelGroup, false, out m_channel);
+
+		if (result != FMOD.RESULT.OK)
+			return false;
+
+		return true;
+	}
+
+	#region Private Functions
+
+	#endregion
+}
