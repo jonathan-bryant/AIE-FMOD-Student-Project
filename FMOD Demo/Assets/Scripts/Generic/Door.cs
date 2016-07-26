0911ac286807cb04d6dc8a29000de35b4affc226
Jonathan Bryant <jonathan-bryant@outlook.com>
2016-07-26 13:52:24 +1000

    Real fake doors

---
+++ FMOD Demo/Assets/Scripts/Generic/Door.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour
+{
+    bool m_doorOpen;
+    public bool DoorOpen { get { return m_doorOpen; } }
+
+    void Start()
+    {
+        
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void Use()
+    {
+        if (!m_doorOpen)
+        {
+            transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), 90.0f);
+            transform.position += new Vector3(2.0f, 0.0f, 2.0f);
+            m_doorOpen = true;
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), -90.0f);
+            transform.position -= new Vector3(2.0f, 0.0f, 2.0f);
+            m_doorOpen = false;
+        }
+    }
+}
