c4a8b7a8e9088c27414e9fae552a5b312184ad5f
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-07-26 13:41:55 +1000

    Shooting gallery can score. 3D track toggle.

---
+++ FMOD Demo/Assets/Scripts/Shooting Gallery/ActionObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public virtual void Use(bool a_use)
+    {
+
+    }
+}
