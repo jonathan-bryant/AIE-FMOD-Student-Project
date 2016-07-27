49119c129eed0fcd9e89f77173c2dd958019ab32
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-07-27 16:37:23 +1000

    Shooting Gallery has music! Rewrote cart script(very messy(Spaghetti code))

--- FMOD Demo/Assets/Scripts/Shooting Gallery/Cart.cs
+++ FMOD Demo/Assets/Scripts/Shooting Gallery/Cart.cs
@@ -4,56 +4,59 @@ using System.Collections.Generic;
 
 public class Cart : ActionObject
 {
-    public Track m_trackHolder;
-    Vector3 m_newHeading;
-    public float m_topSpeed = 3.0f;
-    public float m_acceleration = 30.0f;
-    public float m_turningPower = 0.4f;
+    public float m_topSpeed = 2.0f;
+    public float m_acceleration = 10.0f;
+    public float m_turningPower = 0.1f;
+    public float m_threshold = 0.5f;
     Vector3 m_currentVelocity;
+
+    public Transform m_seat;
+
+    public Track m_trackHolder;
     List<Transform> m_tracks;
     int m_currentTrackIndex;
-    public Transform m_seat;
-    public float m_threshold = 1.0f;
+
     bool m_active;
     public int m_Rounds;
-    int m_currentRound;
+    public int m_currentRound;
     public int CurrentRound { get { return m_currentRound; } }
-    public ScoreBoard m_scoreBoard;
 
+    public SG_MainSpeaker m_mainSpeaker;
+    public ScoreBoard m_scoreBoard;
     public BaseTarget[] m_targets;
 
     void Start()
     {
-        m_currentRound = 1;
+        m_currentRound = 0;
         m_active = false;
         m_tracks = new List<Transform>();
-        //Add children from track holder to m_tracks(list of transforms) 
         for (int i = 0; i < m_trackHolder.transform.childCount; ++i)
         {
             m_tracks.Add(m_trackHolder.transform.GetChild(i));
         }
         m_currentTrackIndex = m_trackHolder.m_startingIndex;
-        //set the position of cart to first tracks position but if track holder is not 3D ignore the y axis.
+
         if (!m_trackHolder.m_3D)
             transform.position = new Vector3(m_tracks[m_currentTrackIndex].transform.position.x, transform.position.y, m_tracks[m_currentTrackIndex].transform.position.z);
         else
             transform.position = m_tracks[m_currentTrackIndex].transform.position;
-
-        //Set the carts forward to the next track in the list.
         transform.forward = m_tracks[m_currentTrackIndex + 1].transform.position - transform.position;
     }
 
     void Update()
     {
+
     }
     void FixedUpdate()
     {
         if (m_active)
         {
-            if (!Finish())
+            CalculateHeading();
+            Move();
+            if(AtStart())
             {
-                CalculateHeading();
-                Move();
+                ResetCart();
+                Pause();
             }
         }
     }
@@ -62,133 +65,57 @@ public class Cart : ActionObject
     {
         if (a_use)
         {
-            //if the cart is not active set it to active
-            if (!m_active)
+            //If player is at starting point restart otherwise play
+            if (AtStart())
             {
-                m_active = true;
-                //if the current track index is equal to the starting index increment it because nothing else will while the cart is finished
-                if (m_currentTrackIndex == m_trackHolder.m_startingIndex)
-                {
-                    if(m_currentRound != m_Rounds)
-                        m_currentTrackIndex++;
-                    Vector3 startPos = m_tracks[m_trackHolder.m_startingIndex].transform.position;
-                    if (!m_trackHolder.m_3D)
-                    {
-                        if (m_currentRound == 1 && transform.position == new Vector3(startPos.x, transform.position.y, startPos.z))
-                        {
-                            m_scoreBoard.ClearScore();
-                            foreach (BaseTarget t in m_targets)
-                            {
-                                t.Reset();
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if (m_currentRound == 1 && transform.position == startPos)
-                        {
-                            m_scoreBoard.ClearScore();
-                            foreach (BaseTarget t in m_targets)
-                            {
-                                t.Reset();
-                            }
-                        }
-                    }
-                }
-                foreach (BaseTarget t in m_targets)
-                {
-                    t.Play();
-                }
-                return;
+                Restart();
             }
+            Play();
         }
-        foreach (BaseTarget t in m_targets)
+        else
         {
-            t.Stop();
+            Pause();
         }
-        m_active = false;
     }
 
-    bool Finish()
+    bool AtStart()
     {
-        //if the cart is at the starting point
-        if (m_currentTrackIndex == m_trackHolder.m_startingIndex)
-        {
-
-            //if the track is not a 3d one leave the y axis to be the carts y axis not the current tracks
-            if (!m_trackHolder.m_3D)
-            {
-                if ((new Vector3(m_tracks[m_trackHolder.m_startingIndex].transform.position.x, transform.position.y, m_tracks[m_trackHolder.m_startingIndex].transform.position.z) - transform.position).magnitude <= m_threshold)
-                {
-                    m_currentRound++;
-                    if (m_currentRound <= m_Rounds)
-                    {
-                        m_currentTrackIndex++;
-                        return false;
-                    }
-
-                    transform.position = new Vector3(m_tracks[m_trackHolder.m_startingIndex].transform.position.x, transform.position.y, m_tracks[m_trackHolder.m_startingIndex].transform.position.z);
-                    Vector3 nextTrack = m_tracks[(m_trackHolder.m_startingIndex + 1 % m_tracks.Count)].transform.position;
-                    transform.forward = (new Vector3(nextTrack.x, 0.0f, nextTrack.z) - transform.position).normalized;
-                    //set the cart to be disabled
-                    m_active = false;
-                    //remove it's velocity
-                    m_currentVelocity = Vector3.zero;
-                    m_currentRound = 1;
-                    foreach (BaseTarget t in m_targets)
-                    {
-                        t.Stop();
-                    }
-                    return true;
-                }
-            }
-            else
-            {
-                if ((m_tracks[m_trackHolder.m_startingIndex].transform.position - transform.position).magnitude <= m_threshold)
-                {
-                    m_currentRound++;
-                    if (m_currentRound <= m_Rounds)
-                    {
-                        m_currentTrackIndex++;
-                        return false;
-                    }
-
-                    transform.position = m_tracks[m_trackHolder.m_startingIndex].transform.position;
-                    transform.forward = (m_tracks[(m_trackHolder.m_startingIndex + 1 % m_tracks.Count)].transform.position - transform.position).normalized;
-                    m_active = false;
-                    m_currentVelocity = Vector3.zero;
-                    m_currentRound = 1;
-                    foreach (BaseTarget t in m_targets)
-                    {
-                        t.Stop();
-                    }
-                    return true;
-                }
-            }
-        }
-        return false;
+        //return true if currentRound is first round and track index is starting index.
+        Vector3 trackPosition = new Vector3(m_tracks[m_trackHolder.m_startingIndex].transform.position.x, transform.position.y, m_tracks[m_trackHolder.m_startingIndex].transform.position.z);
+        return ((m_currentRound == 0 || m_currentRound == m_Rounds) && (m_currentTrackIndex == m_trackHolder.m_startingIndex) && ((trackPosition - transform.position).magnitude <= m_threshold));
     }
     void CalculateHeading()
     {
+        bool change = false;
         //If track is not 3D check distances using the carts y axis instead of the tracks y axis.
         if (!m_trackHolder.m_3D)
         {
             //if the current target track has been reached within the threshold. If it has then increment it or set it back to 0 if last track in array has been reached.
-            if ((new Vector3(m_tracks[m_currentTrackIndex].transform.position.x, transform.position.y, m_tracks[m_currentTrackIndex].transform.position.z) - transform.position).magnitude <= m_threshold)
+            Vector3 trackPosition = new Vector3(m_tracks[m_trackHolder.m_startingIndex].transform.position.x, transform.position.y, m_tracks[m_trackHolder.m_startingIndex].transform.position.z);
+            if ((trackPosition - transform.position).magnitude <= m_threshold)
             {
-                ++m_currentTrackIndex;
-                if (m_currentTrackIndex >= m_trackHolder.transform.childCount)
-                    m_currentTrackIndex = 0;
+                change = true;
             }
         }
         else
         {
             if ((m_tracks[m_currentTrackIndex].transform.position - transform.position).magnitude <= m_threshold)
             {
-                ++m_currentTrackIndex;
-                if (m_currentTrackIndex >= m_trackHolder.transform.childCount)
-                    m_currentTrackIndex = 0;
+                change = true;
             }
+        }
+        if (change)
+        {
+            ++m_currentTrackIndex;
+            //When at the startpoint increment current round
+            if (m_currentTrackIndex == m_tracks.Count - 1)
+            {
+                m_currentRound++;
+                m_mainSpeaker.SetRound(m_currentRound);
+            }
+            //Wrap around track index if it gets out of range of the size of tracks count
+            m_currentTrackIndex %= (m_tracks.Count - 1);
+            
         }
 
         //Turn towards next track
@@ -197,8 +124,8 @@ public class Cart : ActionObject
         //If the track is not 3D then set the y direction to 0
         if (!m_trackHolder.m_3D)
             newForward.y = 0.0f;
+        //Set the carts forward to the new forward
         transform.forward = newForward;
-        Debug.DrawLine(transform.position, transform.position + transform.forward * 2.0f);
     }
     void Move()
     {
@@ -209,5 +136,60 @@ public class Cart : ActionObject
             m_currentVelocity = m_currentVelocity.normalized * m_topSpeed;
         //Add to position with velocity
         transform.position += m_currentVelocity * Time.fixedDeltaTime;
+    }
+    void Play()
+    {
+        m_active = true;
+        m_mainSpeaker.IsActive(true);
+        foreach (BaseTarget t in m_targets)
+        {
+            t.Play();
+        }
+    }
+    void Pause()
+    {
+        m_active = false;
+        m_mainSpeaker.IsActive(false);
+        foreach (BaseTarget t in m_targets)
+        {
+            t.Stop();
+        }
+    }
+    void Restart()
+    {
+        //Set round
+        m_currentRound = 0;
+        //Clear score
+        m_scoreBoard.ClearScore();
+
+        ResetCart();
+
+        //Reset targets
+        foreach (BaseTarget t in m_targets)
+        {
+            t.Reset();
+        }
+        //Reset game result
+        m_mainSpeaker.SetGameResult(0);
+        //Reset round
+        m_mainSpeaker.SetRound(m_currentRound);
+    }
+    void ResetCart()
+    {
+        //Set trackindex to start
+        m_currentTrackIndex = m_trackHolder.m_startingIndex;
+        //Set position
+        if (!m_trackHolder.m_3D)
+        {
+            transform.position = new Vector3(m_tracks[m_currentTrackIndex].transform.position.x, transform.position.y, m_tracks[m_currentTrackIndex].transform.position.z);
+        }
+        else
+        {
+            transform.position = m_tracks[m_currentTrackIndex].transform.position;
+        }
+        //Set forward
+        transform.forward = m_tracks[m_currentTrackIndex + 1].transform.position - transform.position;
+        //Zero out currentVelocity
+        m_currentVelocity = Vector3.zero;
     }
 }
