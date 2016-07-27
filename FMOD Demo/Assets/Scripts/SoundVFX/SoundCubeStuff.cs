d16dfc4aebcaa607bb49b757ba3c28487721467a
Cameron Baron <twitchin86@gmail.com>
2016-07-27 17:06:26 +1000

    Added VFX scene and playing around with shaders/effects.

---
+++ FMOD Demo/Assets/Scripts/SoundVFX/SoundCubeStuff.cs
@@ -0,0 +1,36 @@
+﻿/*=================================================================
+Project:		#PROJECTNAME#
+Developer:		#DEVOLPERNAME#
+Company:		#COMPANY#
+Date:			#CREATIONDATE#
+==================================================================*/
+
+using UnityEngine;
+
+
+public class SoundCubeStuff : MonoBehaviour 
+{
+    // Public Vars
+
+    // Private Vars
+    Material material;
+
+	void Start () 
+	{
+        Vector3 uvPos = (transform.position - transform.parent.parent.position);
+        material = GetComponent<Renderer>().material;
+        uvPos.x = 1.0f / (Mathf.Abs(uvPos.x)) * -1.0f;
+        material.SetFloat("UVx", uvPos.x);
+        uvPos.z = 1.0f / (Mathf.Abs(uvPos.z)) * -1.0f;
+        material.SetFloat("UVy", uvPos.z);
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	#region Private Functions
+
+	#endregion
+}
