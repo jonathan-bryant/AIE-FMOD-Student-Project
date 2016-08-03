d34170af80522af97a9831fe2049f901cc42cfcc
Cameron Baron <twitchin86@gmail.com>
2016-08-03 13:18:49 +1000

    - Added Async loading Shooting gallery from Overworld - Unloading currently freezes Unity (hopefully just the pc's at Fmod)

---
+++ FMOD Demo/Assets/Scripts/Generic/SceneLoading/SceneTrigger.cs
@@ -0,0 +1,44 @@
+﻿/*=================================================================
+Project:		AIE FMOD
+Developer:		Cameron Baron
+Company:		#COMPANY#
+Date:			03/08/2016
+==================================================================*/
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTrigger : MonoBehaviour 
+{
+    // Public Vars
+
+    // Private Vars
+      public string m_sceneToLoad;
+    int m_sceneIndex;
+
+	void Start () 
+	{
+        if (m_sceneToLoad == null)
+        {
+            Debug.LogError("No scene attached to " + gameObject.name);
+            Destroy(this);
+        }
+	}
+	
+	void Update () 
+	{
+	
+	}
+
+	#region Private Functions
+
+    void OnTriggerEnter()
+    {
+        if (!SceneManager.GetSceneByName(m_sceneToLoad).isLoaded)
+        {
+            SceneManager.LoadSceneAsync(m_sceneToLoad, LoadSceneMode.Additive);
+        }
+    }
+
+	#endregion
+}
---
+++ FMOD Demo/Assets/Scripts/Generic/SceneLoading/UnloadRoomsTrigger.cs
@@ -0,0 +1,40 @@
+﻿/*=================================================================
+Project:		AIE FMOD
+Developer:		Cameron Baron
+Company:		#COMPANY#
+Date:			03/08/2016
+==================================================================*/
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UnloadRoomsTrigger : MonoBehaviour
+{
+    // Public Vars
+
+    // Private Vars
+    int m_sceneCount;
+
+    void Start()
+    {
+
+    }
+
+    #region Private Functions
+
+    void OnTriggerEnter()
+    {
+        m_sceneCount = SceneManager.sceneCount;
+        for (int index = 0; index < m_sceneCount; index++)
+        {
+            Scene indexScene = SceneManager.GetSceneAt(index);
+            if (indexScene.isLoaded && indexScene.name != "Overworld")
+            {
+                Debug.Log("Unloading: " + indexScene.name);
+                //SceneManager.UnloadScene(indexScene.name);    // Breaks computer (@Fmod, possibly bad pc's)
+            }
+        }
+    }
+
+    #endregion
+}
