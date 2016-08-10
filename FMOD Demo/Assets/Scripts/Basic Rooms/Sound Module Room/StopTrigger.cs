a3f6a6bbf1c1ff522157befb36a99648c6f07bf6
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-08-10 17:08:33 +1000

    Working on player parameter change room.

---
+++ FMOD Demo/Assets/Scripts/Basic Rooms/Sound Module Room/StopTrigger.cs
@@ -0,0 +1,34 @@
+﻿/*=================================================================
+Project:		AIE FMOD
+Developer:		Cameron Baron
+Company:		#COMPANY#
+Date:			02/08/2016
+==================================================================*/
+
+using UnityEngine;
+
+
+public class StopTrigger : ActionObject
+{
+    // Public Vars
+
+    // Private Vars
+    CreationTriggers m_triggerScript;
+
+    void Start()
+    {
+        m_triggerScript = GetComponentInParent<CreationTriggers>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            m_triggerScript.Stop();
+        }
+    }
+
+    #region Private Functions
+
+    #endregion
+}
