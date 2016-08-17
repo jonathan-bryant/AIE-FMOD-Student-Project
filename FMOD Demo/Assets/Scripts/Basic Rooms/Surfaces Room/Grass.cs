e0677de8a65b24dc32526b045787cf23d7807264
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-08-17 14:00:18 +1000

    Surfaces room has a treadmill. Waiting on art and description boxes ui.

---
+++ FMOD Demo/Assets/Scripts/Basic Rooms/Surfaces Room/Grass.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grass : MonoBehaviour {
+
+    //rotate grass
+    //Random platform animation
+    bool m_standing;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter()
+    {
+
+    }
+}
--- FMOD Demo/Assets/Scripts/Basic Rooms/Surfaces Room/TreadmillSpawner.cs
+++ FMOD Demo/Assets/Scripts/Basic Rooms/Surfaces Room/TreadmillSpawner.cs
@@ -6,28 +6,33 @@ public class TreadmillSpawner : MonoBehaviour
 {
 
     public GameObject[] m_surfaces;
-    int m_index;
-    float m_elapsed;
     public float m_speed;
+    public int m_numOfTiles;
+    public int m_numOfRepititions;
+    public List<GameObject> m_gears;
 
-    public List<GameObject> m_floors;
+    int m_index;
 
-    // Use this for initialization
+    List<GameObject> m_floors;
+    
     void Start()
     {
-        AddFloor(transform.position + transform.right * 10.0f);
-    }
-
-    // Update is called once per frame
+        m_floors = new List<GameObject>();
+        for (int i = m_numOfTiles - 1; i >= 0; --i)
+        {
+            AddFloor(transform.position + transform.forward * m_surfaces[0].transform.localScale.z * i);
+        }
+    }    
     void Update()
     {
-
-
+        MoveFloors();
+        MoveGears();
     }
 
     void AddFloor(Vector3 a_position)
     {
-        GameObject floor = Instantiate(m_surfaces[m_index++]);
+        int index = m_index / m_numOfRepititions;
+        GameObject floor = Instantiate(m_surfaces[Mathf.Clamp(index, 0, 2)]);
         if (a_position == Vector3.zero)
         {
             floor.transform.position = transform.position;
@@ -36,12 +41,37 @@ public class TreadmillSpawner : MonoBehaviour
         {
             floor.transform.position = a_position;
         }
-        m_index %= 3;
+        m_index++;
+        m_index %= 3 * m_numOfRepititions;
         m_floors.Add(floor);
     }
     void MoveFloors()
     {
-
+        for (int i = 0; i < m_floors.Count; ++i)
+        {
+            m_floors[i].transform.position += transform.forward * m_speed * Time.deltaTime;
+        }
+        if (Mathf.Abs(m_floors[0].transform.position.z - transform.position.z) >= m_surfaces[0].transform.localScale.z * (m_numOfTiles - 1))
+        {
+            Destroy(m_floors[0]);
+            m_floors.RemoveAt(0);
+            AddFloor(m_floors[0].transform.position - transform.forward * m_surfaces[0].transform.localScale.z * (m_numOfTiles - 1));
+        }
     }
-}
+    void MoveGears()
+    {
+        for(int i = 0; i < m_gears.Count; ++i)
+        {
+            m_gears[i].transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f), -m_speed / (m_gears[i].transform.localScale.x * 0.5f));
+        }
+    }
 
+    void OnTriggerStay(Collider a_col)
+    {
+        if(a_col.gameObject.tag == "Player")
+        {
+            a_col.gameObject.transform.position += transform.forward * m_speed * Time.deltaTime;
+        }
+    }
+
+}
