0d08ffabbf9207cf6273ecff97f7b0c6a1e1a9af
Cameron Baron <twitchin86@gmail.com>
2016-08-17 12:25:22 +1000

    Added Editor options for billboarding and minimum alpha value.

--- FMOD Demo/Assets/Scripts/Generic/UI/DescriptionCanvasScript.cs
+++ FMOD Demo/Assets/Scripts/Generic/UI/DescriptionCanvasScript.cs
@@ -14,22 +14,25 @@ public class DescriptionCanvasScript : MonoBehaviour
 {
 	public bool m_active = false;
 	public float m_timeUntilFade = 2.0f;
+    public bool m_billboard = true;
+    [Range(0, 1)]    public float m_minAlpha = 0.0f;
 
 	public CanvasGroup m_canvas;
     List<Image> images;
 	void Start ()
 	{
 		m_canvas = GetComponentInChildren<CanvasGroup>();
-        m_canvas.alpha = 0.5f;
+        m_canvas.alpha = m_minAlpha;
     }
 	
 
 	void Update ()
     {
         // Make canvas billboard to camera.
-        transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Vector3.up);
+        if (m_billboard)
+            transform.LookAt(transform.position + Camera.main.transform.rotation * Vector3.forward, Vector3.up);
 
-        if (!m_active && m_canvas.alpha > 0.5f)
+        if (!m_active && m_canvas.alpha > m_minAlpha)
 		{
 			m_canvas.alpha -= Time.deltaTime / 2.0f;
 		}
