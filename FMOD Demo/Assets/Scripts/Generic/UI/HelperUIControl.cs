37b45494765611acb2225495cf876363674866d9
Cameron Baron <twitchin86@gmail.com>
2016-08-23 15:54:21 +1000

    Adding communication/helper UI prefab and look at script

--- FMOD Demo/Assets/Scripts/Generic/UI/HelperUIControl.cs
+++ FMOD Demo/Assets/Scripts/Generic/UI/HelperUIControl.cs
@@ -12,38 +12,46 @@ using UnityEngine.EventSystems;
 
 public enum HELPERSTATE
 {
-    CLOSED,
     STOPPED,
     OPENING,
     LOADING,
+    IDLE
 };
 
 public class HelperUIControl : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [Range(0, 10.0f)]    public float m_timeUntilOpen = 2.0f;
-    [Range(0, 10.0f)]    public float m_timeUntilClosed = 2.0f;
-    [Range(0, 30.0f)]    public float m_maxPlayerDistance = 10.0f;
+    [Range(0, 10.0f)]
+    public float m_timeUntilOpen = 2.0f;
+    [Range(0, 10.0f)]
+    public float m_timeUntilClosed = 2.0f;
+    [Range(0, 30.0f)]
+    public float m_maxPlayerDistance = 10.0f;
     public bool m_billboard = true;
+    public string m_uiLoadAnim = "UI Prompt Load";
+    public string m_uiOpenAnim = "UI Prompt Open";
+    public string m_uiCloseAnim = "UI Prompt Close";
 
     bool m_updateFacing = false;
-    HELPERSTATE m_currentState = HELPERSTATE.CLOSED;
+    HELPERSTATE m_currentState = HELPERSTATE.IDLE;
 
     GameObject m_playerRef = null;     // Used for getting the players position to billboard the UI.
-    Animation m_uiAnimation;
+    Animator m_uiAnimator;
 
-	void Start () 
-	{
+    void Start()
+    {
         m_playerRef = GameObject.FindGameObjectWithTag("Player");
-        m_uiAnimation = GetComponent<Animation>();
-	}
-	
-	void Update () 
-	{
+        m_uiAnimator = GetComponentInChildren<Animator>();
+        StopAnimation();
+    }
+
+    void Update()
+    {
         if (m_billboard && m_updateFacing)
         {
-            transform.LookAt(Vector3.Lerp(transform.forward, m_playerRef.transform.position, 0.5f), Vector3.up);    // Lerp the facing direction to make smooth.
-        }	
-	}
+            transform.LookAt(m_playerRef.transform.position, Vector3.up);    // Lerp the facing direction to make smooth.
+            transform.Rotate(Vector3.up, 180.0f);
+        }
+    }
 
     void LateUpdate()
     {
@@ -53,47 +61,47 @@ public class HelperUIControl : MonoBehaviour, IPointerEnterHandler, IPointerExit
         }
     }
 
-    public void OpenHelper()
-    {
-        // Play opening animation
-        m_currentState = HELPERSTATE.OPENING;
-        OpenWaitAndLoad();
-    }
-
     public void LoadHelper()
     {
         // Play animation to load
+        m_uiAnimator.SetTime(0.0f);
+        m_uiAnimator.Play(m_uiLoadAnim, 0, 0.0f);
+        PlayForward();
         m_currentState = HELPERSTATE.LOADING;
+        StartCoroutine(LoadWaitAndOpen());
+    }
+
+    IEnumerator LoadWaitAndOpen()
+    {
+        while (AnimationProgress() < 0.90f)
+        {
+            if (m_currentState != HELPERSTATE.LOADING)
+            {
+                Debug.Log("Break: " + m_currentState);
+                break;
+            }
+            yield return null;
+        }
+        if (m_currentState == HELPERSTATE.LOADING)
+            OpenHelper();
+    }
+    public void OpenHelper()
+    {
+        // Play opening animation
+        m_uiAnimator.Play(m_uiOpenAnim, 0);
+        m_currentState = HELPERSTATE.OPENING;
     }
 
     public void StopHelper()
     {
-        if (m_uiAnimation.isPlaying)
+        // If the ui is not opening (allows the ui to open fully after loading)
+        if (m_uiAnimator.GetCurrentAnimatorStateInfo(0).IsName(m_uiLoadAnim) || 
+            (m_uiAnimator.GetCurrentAnimatorStateInfo(0).IsName(m_uiOpenAnim) && m_uiAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1))
         {
-            m_uiAnimation.Stop();
+            Debug.Log("STOPHELPER()");
+            StopAnimation();
             m_currentState = HELPERSTATE.STOPPED;
-            WaitAndCloseHelper();
-        }
-    }
-
-    public void CloseHelper()
-    {
-        // Play animation in reverse from current position
-        m_uiAnimation[""].speed = -1;
-        m_uiAnimation.Play("");
-        m_currentState = HELPERSTATE.CLOSED;
-    }
-
-    IEnumerator OpenWaitAndLoad()
-    {
-        while (m_uiAnimation.isPlaying)
-        {
-            if (m_currentState != HELPERSTATE.OPENING)
-            {
-                break;
-            }
-            yield return null;
-            LoadHelper();
+            StartCoroutine(WaitAndCloseHelper());
         }
     }
 
@@ -102,17 +110,41 @@ public class HelperUIControl : MonoBehaviour, IPointerEnterHandler, IPointerExit
         while (m_currentState == HELPERSTATE.STOPPED)
         {
             yield return new WaitForSeconds(m_timeUntilClosed);
-            CloseHelper();
+
+            // Play animation in reverse from current position
+            if (m_uiAnimator.GetCurrentAnimatorStateInfo(0).IsName(m_uiLoadAnim))
+            {
+                PlayBackwards();
+                while (m_uiAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0)
+                {
+                    yield return null;
+                }
+            }
+            else if (m_uiAnimator.GetCurrentAnimatorStateInfo(0).IsName(m_uiOpenAnim) &&
+                m_uiAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.9f)
+            {
+                m_uiAnimator.Play(m_uiCloseAnim, 0, 0f);
+                PlayForward();
+                while (m_uiAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1)
+                {
+                    yield return null;
+                }
+            }
+
+            m_currentState = HELPERSTATE.IDLE;
         }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        Debug.Log(m_currentState);
         switch (m_currentState)
         {
-            case HELPERSTATE.CLOSED: OpenHelper();
+            case HELPERSTATE.IDLE:
+                //if (m_uiAnimator.GetCurrentAnimatorStateInfo(0).IsName(m_uiCloseAnim))
+                    LoadHelper();
                 break;
-            case HELPERSTATE.STOPPED: m_uiAnimation.Play();
+            case HELPERSTATE.STOPPED:
                 break;
             case HELPERSTATE.OPENING:
                 break;
@@ -126,5 +158,35 @@ public class HelperUIControl : MonoBehaviour, IPointerEnterHandler, IPointerExit
     public void OnPointerExit(PointerEventData eventData)
     {
         StopHelper();
+    }
+
+    void PlayForward()
+    {
+        m_uiAnimator.SetFloat("Speed", 1);
+        m_uiAnimator.speed = 1;
+    }
+
+    void PlayBackwards()
+    {
+        m_uiAnimator.SetFloat("Speed", -1);
+        m_uiAnimator.speed = 1;
+    }
+
+    void StopAnimation()
+    {
+        m_uiAnimator.speed = 0;
+    }
+
+    float AnimationProgress()
+    {
+        float totalTime = m_uiAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+        float fractionalTime = totalTime - (int)totalTime;
+
+        if (totalTime >= 1 && fractionalTime == 0.0f)
+        {
+            return 1.0f;
+        }
+
+        return Mathf.Abs( fractionalTime );
     }
 }
