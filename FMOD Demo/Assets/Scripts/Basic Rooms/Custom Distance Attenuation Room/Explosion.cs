2f49136ac186bbac08d3eaa65d9631d41b6893fc
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-08-23 13:55:53 +1000

    Changed player parameter room to have a pedal, change gears, add load, and ignition. Currently changing custom distance attenuation room. Fixe surfaces room not showing the right floor parameter value.

---
+++ FMOD Demo/Assets/Scripts/Basic Rooms/Custom Distance Attenuation Room/Explosion.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour
+{
+    public ParticleSystem m_particle;
+
+    /*===============================================Fmod====================================================
+    |   This piece of code will allow the string m_footstepSurfaceName to use the event browser to select   |
+    |   the event, in the inspector.                                                                        |
+    =======================================================================================================*/
+    [FMODUnity.EventRef]
+
+    /*===============================================Fmod====================================================
+    |   Name of Event. Used in conjunction with EventInstance.                                              |
+    =======================================================================================================*/
+    public string m_footstepSurfaceName;
+
+    float m_elapsed;
+
+	void Start () {
+
+	}
+	void Update () {
+        m_elapsed += Time.deltaTime;
+        if(m_elapsed >= 6.0f)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(m_footstepSurfaceName, transform.position);
+            m_elapsed = 0.0f;
+            m_particle.Play();
+        }
+	}
+}
