ac720069b4e45794861f75a698d901fa0ccbfe10
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-08-29 16:32:00 +1000

    Guide Added Minimum.

---
+++ FMOD Demo/Assets/Scripts/Generic/GuideHelper.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GuideHelper : ActionObject
+{
+    public Guide m_spinGuide;
+    public Guide m_interactiveMusicGuide;
+    public Guide m_snapshotGuide;
+    public Guide m_playerParamGuide;
+
+    void Start ()
+    {
+	}
+	void Update ()
+    {
+
+	}
+
+    public override void ActionDown(GameObject sender)
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            m_spinGuide.Play();
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            m_interactiveMusicGuide.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            m_snapshotGuide.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            m_playerParamGuide.Play();
+        }
+    }
+}
