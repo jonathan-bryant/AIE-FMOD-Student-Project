2ca8e04d0f0a740930449a426cd4705954010524
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-08-30 13:45:51 +1000

    added elevator. needs sound and art.

--- FMOD Demo/Assets/Scripts/Basic Rooms/Sound Spin Room/ElevatorButton.cs
+++ FMOD Demo/Assets/Scripts/Basic Rooms/Sound Spin Room/ElevatorButton.cs
@@ -3,69 +3,18 @@ using System.Collections;
 
 public class ElevatorButton : ActionObject
 {
-    public GameObject m_elevator;
-    public GameObject m_doors;
-    public FMODUnity.StudioEventEmitter m_event;
-    bool m_isActive;
-    public float m_duration;
-    float m_elapsed;
-
-    float m_originalZ;
-    bool m_doorsOpen;
+    public Elevator m_elevator;
+    public int m_floor;
 
     void Start()
     {
-        m_isActive = false;
-        m_elapsed = 0.0f;
-        m_originalZ = m_elevator.transform.position.z;
-        m_doorsOpen = true;
     }
     void Update()
     {
-        m_elapsed += Time.deltaTime;
-        if(m_isActive)
-        {
-            if (m_doorsOpen)
-            {
-                if (m_elapsed >= 1.0f)
-                {
-                    m_doorsOpen = false;
-                    m_event.SetParameter("Intensity", 0);
-                    m_event.Play();
-                }
-            }
-            else
-            {
-                if (m_elapsed >= m_duration)
-                {
-                    m_isActive = false;
-                    m_elapsed = 0.0f;
-                    m_event.SetParameter("Intensity", 1);
-                }
-                else
-                {
-                    Vector3 position = m_elevator.transform.position;
-                    position.z = (Mathf.Sin(Time.time * 10.0f) * 0.05f) + m_originalZ;
-                    m_elevator.transform.position = position;
-                }
-            }
-        }
-        else
-        {
-            if (m_elapsed >= 1.0f)
-            {
-                //m_doors.Open();
-                m_doorsOpen = true;
-            }
-        }
     }
+
     public override void ActionDown(GameObject sender)
     {
-        if (!m_isActive)
-        {
-            m_isActive = true;
-            m_elapsed = 0.0f;
-            //m_doors.Shut();
-        }
+        m_elevator.ChangeFloor(m_floor);
     }
 }
---
+++ FMOD Demo/Assets/Scripts/Basic Rooms/Sound Spin Room/ElevatorDoor.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorDoor : MonoBehaviour
+{
+    public GameObject m_leftDoor, m_rightDoor;
+    int m_isActive;
+    bool m_doorsOpen;
+
+    void Start()
+    {
+        m_isActive = 0;
+        m_doorsOpen = true;
+    }
+    void Update()
+    {
+        if (m_isActive == 1)
+        {
+            //open   
+            if (!m_doorsOpen)
+            {
+                Vector3 leftDoorPos = m_leftDoor.transform.localPosition;
+                Vector3 rightDoorPos = m_rightDoor.transform.localPosition;
+
+                leftDoorPos.z -= Time.deltaTime;
+                rightDoorPos.z += Time.deltaTime;
+                if (leftDoorPos.z < -1.4f)
+                {
+                    leftDoorPos.z = -1.4f;
+                    rightDoorPos.z = 1.4f;
+                    m_doorsOpen = true;
+                    m_isActive = 0;
+                }
+                m_leftDoor.transform.localPosition = leftDoorPos;
+                m_rightDoor.transform.localPosition = rightDoorPos;
+            }
+        }
+        else if (m_isActive == -1)
+        {
+            //close
+            if (m_doorsOpen)
+            {
+                Vector3 leftDoorPos = m_leftDoor.transform.localPosition;
+                Vector3 rightDoorPos = m_rightDoor.transform.localPosition;
+
+                leftDoorPos.z += Time.deltaTime;
+                rightDoorPos.z -= Time.deltaTime;
+                if (leftDoorPos.z > -0.5f)
+                {
+                    leftDoorPos.z = -0.5f;
+                    rightDoorPos.z = 0.5f;
+                    m_doorsOpen = false;
+                    m_isActive = 0;
+                }
+                m_leftDoor.transform.localPosition = leftDoorPos;
+                m_rightDoor.transform.localPosition = rightDoorPos;
+            }
+        }
+    }
+
+    public void OpenDoors()
+    {
+        m_isActive = 1;
+    }
+    public void CloseDoors()
+    {
+        m_isActive = -1;
+    }
+}
