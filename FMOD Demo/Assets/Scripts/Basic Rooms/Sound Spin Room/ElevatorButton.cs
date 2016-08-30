c5f295badac86468cfc10b38bdeb0f9e2d541706
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-08-30 10:58:57 +1000

    Elevator rooms on multiple rooms.

--- FMOD Demo/Assets/Scripts/Basic Rooms/Sound Spin Room/ElevatorButton.cs
+++ FMOD Demo/Assets/Scripts/Basic Rooms/Sound Spin Room/ElevatorButton.cs
@@ -3,31 +3,59 @@ using System.Collections;
 
 public class ElevatorButton : ActionObject
 {
+    public GameObject m_elevator;
+    public GameObject m_doors;
     public FMODUnity.StudioEventEmitter m_event;
     bool m_isActive;
     public float m_duration;
     float m_elapsed;
 
+    float m_originalZ;
+    bool m_doorsOpen;
+
     void Start()
     {
         m_isActive = false;
         m_elapsed = 0.0f;
+        m_originalZ = m_elevator.transform.position.z;
+        m_doorsOpen = true;
     }
-
-    // Update is called once per frame
     void Update()
     {
+        m_elapsed += Time.deltaTime;
         if(m_isActive)
         {
-            m_elapsed += Time.deltaTime;
-            if (m_elapsed >= m_duration)
+            if (m_doorsOpen)
             {
-                m_isActive = false;
-                m_event.SetParameter("Intensity", 1);
+                if (m_elapsed >= 1.0f)
+                {
+                    m_doorsOpen = false;
+                    m_event.SetParameter("Intensity", 0);
+                    m_event.Play();
+                }
             }
             else
             {
-                //Wiggle elevator
+                if (m_elapsed >= m_duration)
+                {
+                    m_isActive = false;
+                    m_elapsed = 0.0f;
+                    m_event.SetParameter("Intensity", 1);
+                }
+                else
+                {
+                    Vector3 position = m_elevator.transform.position;
+                    position.z = (Mathf.Sin(Time.time * 10.0f) * 0.05f) + m_originalZ;
+                    m_elevator.transform.position = position;
+                }
+            }
+        }
+        else
+        {
+            if (m_elapsed >= 1.0f)
+            {
+                //m_doors.Open();
+                m_doorsOpen = true;
             }
         }
     }
@@ -35,10 +63,9 @@ public class ElevatorButton : ActionObject
     {
         if (!m_isActive)
         {
-            m_event.SetParameter("Intensity", 0);
-            m_event.Play();
             m_isActive = true;
             m_elapsed = 0.0f;
+            //m_doors.Shut();
         }
     }
 }
