d7588f82ab1e3802b98dd99e551a2d9935c70e39
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-08-31 15:20:52 +1000

    no message

--- FMOD Demo/Assets/Scripts/Generic/Menu/Menu.cs
+++ FMOD Demo/Assets/Scripts/Generic/Menu/Menu.cs
@@ -19,33 +19,57 @@ public class Menu : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Escape))
         {
-            
+            m_menuIsOpen = !m_menuIsOpen;
         }
     }
 
     public void OptionsClick()
     {
-        if(!m_optionsOpen)
-            m_animator.Play("Options Open");
-        else
+        if (m_menuIsOpen)
+        {
+            if (!m_optionsOpen)
+                m_animator.Play("Options Open");
+            else
+                m_animator.Play("Options Close");
+            m_optionsOpen = !m_optionsOpen;
+        }
+        else if(m_optionsOpen)
+        {
             m_animator.Play("Options Close");
-        m_optionsOpen = !m_optionsOpen;
+            m_optionsOpen = false;
+        }
     }
     public void PracticalClick()
     {
-        if (!m_practicalOpen)
+        if (m_menuIsOpen)
+        {
+            if (!m_practicalOpen)
             m_animator.Play("Practical Room Open");
         else
             m_animator.Play("Practical Room Close");
         m_practicalOpen = !m_practicalOpen;
+        }
+        else if (m_practicalOpen)
+        {
+            m_animator.Play("Practical Room Close");
+            m_practicalOpen = false;
+        }
     }
     public void BasicClick()
     {
-        if (!m_basicOpen)
+        if (m_menuIsOpen)
+        {
+            if (!m_basicOpen)
             m_animator.Play("Basic Room Open");
         else
             m_animator.Play("Basic Room Close");
-        m_basicOpen = !m_basicOpen;
+            m_basicOpen = !m_basicOpen;
+        }
+        else if (m_basicOpen)
+        {
+            m_animator.Play("Basic Room Close");
+            m_basicOpen = false;
+        }
     }
     public void QuitClick()
     {
