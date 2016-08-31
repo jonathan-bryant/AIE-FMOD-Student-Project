c6bd763c97481f8de46d42413ec60eaac884ce32
Cameron Baron <info@cameronbaron.com>
2016-08-31 17:09:26 +1000

    Added FMOD pillar logo and spinning script.

---
+++ FMOD Demo/Assets/Scripts/Generic/PillarLogoSpin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+
+public class PillarLogoSpin : MonoBehaviour 
+{
+	
+	void Update () 
+	{
+        transform.Rotate(Vector3.up, Time.deltaTime * 4);
+	}
+
+	#region Private Functions
+
+	#endregion
+}
