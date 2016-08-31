e923dbfe92caf0bd4d309771086a68292e852569
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-08-31 13:53:32 +1000

    no message

--- FMOD Demo/Assets/Scripts/Generic/Menu/Menu.cs
+++ FMOD Demo/Assets/Scripts/Generic/Menu/Menu.cs
@@ -4,16 +4,38 @@ using System.Collections;
 public class Menu : MonoBehaviour
 {
     bool m_isPaused;
+    int m_option;
     Animator m_animator;
-	void Start () {
+    void Start()
+    {
         m_animator = GetComponent<Animator>();
-	}
-	
-	void Update () {
+    }
+
+    void Update()
+    {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             m_isPaused = !m_isPaused;
-            m_animator.Play(1);
+            m_animator.SetFloat("Speed", m_isPaused ? 1 : -1);
+            m_animator.Play("Pause Open");
         }
-	}
+        if (m_isPaused)
+        {
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                m_animator.SetFloat("Speed", 1);
+                m_animator.Play("Options Open");
+            }
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                m_animator.SetFloat("Speed", 1);
+                m_animator.Play("Basic Room Open");
+            }
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                m_animator.SetFloat("Speed", 1);
+                m_animator.Play("Practical Room Open");
+            }
+        }
+    }
 }
