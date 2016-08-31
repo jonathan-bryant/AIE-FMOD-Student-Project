29c1cfeb10acb14ce713338ea0c2155f0afcd720
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-08-31 14:48:11 +1000

    no message

--- FMOD Demo/Assets/Scripts/Generic/Menu/Menu.cs
+++ FMOD Demo/Assets/Scripts/Generic/Menu/Menu.cs
@@ -4,38 +4,53 @@ using System.Collections;
 public class Menu : MonoBehaviour
 {
     bool m_isPaused;
-    int m_option;
+    enum Selection
+    {
+        Pause,
+        Practical,
+        Basic,
+        Option
+    }
+    Selection m_currentSelection;
     Animator m_animator;
+    bool m_finishedAnimation;
     void Start()
     {
         m_animator = GetComponent<Animator>();
+        m_finishedAnimation = true;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (m_finishedAnimation)
         {
-            m_isPaused = !m_isPaused;
-            m_animator.SetFloat("Speed", m_isPaused ? 1 : -1);
-            m_animator.Play("Pause Open");
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                m_isPaused = !m_isPaused;
+                m_animator.Play("Pause Open", 0, m_isPaused ? 0 : 1);
+                m_currentSelection = Selection.Pause;
+            }
+            if (m_isPaused)
+            {
+                if (Input.GetKeyDown(KeyCode.O))
+                {
+                    if (m_currentSelection == Selection.Options)
+                        m_animator.Play("Options Close");
+                    m_animator.Play("Options Open");
+                }
+                if (Input.GetKeyDown(KeyCode.B))
+                {
+                    m_animator.Play("Basic Room Open");
+                }
+                if (Input.GetKeyDown(KeyCode.P))
+                {
+                    m_animator.Play("Practical Room Open");
+                }
+            }
         }
-        if (m_isPaused)
-        {
-            if (Input.GetKeyDown(KeyCode.O))
-            {
-                m_animator.SetFloat("Speed", 1);
-                m_animator.Play("Options Open");
-            }
-            if (Input.GetKeyDown(KeyCode.B))
-            {
-                m_animator.SetFloat("Speed", 1);
-                m_animator.Play("Basic Room Open");
-            }
-            if (Input.GetKeyDown(KeyCode.P))
-            {
-                m_animator.SetFloat("Speed", 1);
-                m_animator.Play("Practical Room Open");
-            }
-        }
+    }
+    public void Stop()
+    {
+        m_animator.SetFloat("Speed", 0.0f);
     }
 }
