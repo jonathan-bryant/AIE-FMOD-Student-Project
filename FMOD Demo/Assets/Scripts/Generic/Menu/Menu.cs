1ec8addd37de0041a8329b8cc6352dfbc9c40240
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-09-05 11:00:59 +1000

    Can tp to rooms but need to set each room in the menu.

--- FMOD Demo/Assets/Scripts/Generic/Menu/Menu.cs
+++ FMOD Demo/Assets/Scripts/Generic/Menu/Menu.cs
@@ -6,8 +6,11 @@ public class Menu : MonoBehaviour
     bool m_menuIsOpen;
     Animator m_animator;
     bool m_optionsOpen, m_practicalOpen, m_basicOpen;
+    ActorControls m_actor;
+
     void Start()
     {
+        m_actor = Camera.main.GetComponentInParent<ActorControls>();
         m_menuIsOpen = false;
         m_animator = GetComponent<Animator>();
         m_optionsOpen = false;
@@ -17,14 +20,18 @@ public class Menu : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             m_menuIsOpen = !m_menuIsOpen;
             if (m_menuIsOpen)
+            {
                 m_animator.Play("Pause Open");
+                m_actor.DisableMovementAndMouse(true);
+            }
             else
             {
                 m_animator.Play("Pause Close");
+                m_actor.DisableMovementAndMouse(false);
                 if (m_optionsOpen)
                 {
                     m_animator.Play("Options Close");
@@ -119,5 +126,19 @@ public class Menu : MonoBehaviour
     public void LogoClick()
     {
         Application.OpenURL("http://http://www.fmod.org/");
+    }
+
+    public void TeleportToRoom(string a_room)
+    {
+        GameObject obj = GameObject.Find(a_room);
+        if(obj)
+        {
+            m_actor.transform.position = obj.transform.position + (-obj.transform.right * 10.0f) + (-obj.transform.forward * 1.266646f) + Vector3.up * 0.7f;
+            Vector3 doorCenter = obj.transform.position + (-obj.transform.forward * 1.266646f);
+
+            m_actor.transform.LookAt(new Vector3(doorCenter.x, 0.0f, doorCenter.z));
+            m_actor.transform.localEulerAngles = new Vector3(0.0f,m_actor.transform.localEulerAngles.y, 0.0f);
+            Camera.main.transform.localEulerAngles = Vector3.zero;
+        }
     }
 }
