dbdf51589077a9fbaba668ec7945c77ffa1e4a97
Cameron Baron <info@cameronbaron.com>
2016-09-07 11:30:48 +1000

    Finished up Pause menu options controls. Sorted FMOD Studio project buses correctly.

---
+++ FMOD Demo/Assets/Scripts/Generic/UI/DropdownSettingsScript.cs
@@ -0,0 +1,75 @@
+﻿/*=================================================================
+Project:		AIE FMOD
+Developer:		Cameron Baron
+Company:		FMOD
+Date:			24/08/2016
+==================================================================*/
+
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DropdownSettingsScript : MonoBehaviour 
+{
+    public Dropdown m_qualityDropdown;
+    public Dropdown m_resolutionDropdown;
+    public Toggle m_fullscreenToggle;
+
+    Resolution[] m_resolutions;
+
+    void Start()
+    {
+        m_qualityDropdown.value = QualitySettings.GetQualityLevel();
+        m_qualityDropdown.onValueChanged.AddListener(delegate { OnQualityValueChange(); });
+
+
+        m_resolutionDropdown.ClearOptions();
+        m_resolutions = Screen.resolutions;
+
+        for (int i = 0; i < m_resolutions.Length; i++)
+        {
+            m_resolutionDropdown.options.Add(new Dropdown.OptionData(m_resolutions[i].ToString()));
+            if (Screen.currentResolution.ToString() == m_resolutions[i].ToString())
+            {
+                Debug.Log("found current res");
+                m_resolutionDropdown.value = i;
+            }
+        }
+        m_resolutionDropdown.onValueChanged.AddListener(delegate { OnResolutionValueChange(); });
+
+        m_fullscreenToggle.isOn = Screen.fullScreen;
+        m_fullscreenToggle.onValueChanged.AddListener(delegate { OnFullscreenToggleChange(); });
+    }
+
+
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+    void OnGUI()
+    {
+        string[] names = QualitySettings.names;
+        GUILayout.BeginVertical();
+        int i = 0;
+        while (i < names.Length)
+        {
+            if (GUILayout.Button(names[i]))
+                QualitySettings.SetQualityLevel(i, true);
+
+            i++;
+        }
+        GUILayout.EndVertical();
+    }
+#endif
+
+    void OnQualityValueChange()
+    {
+        QualitySettings.SetQualityLevel(m_qualityDropdown.value);
+    }
+
+    void OnResolutionValueChange()
+    {
+        Screen.SetResolution(m_resolutions[m_resolutionDropdown.value].width, m_resolutions[m_resolutionDropdown.value].height, m_fullscreenToggle.isOn);
+    }
+
+    void OnFullscreenToggleChange()
+    {
+        Screen.fullScreen = m_fullscreenToggle.isOn;
+    }
+}
--- FMOD Demo/Assets/Scripts/Generic/UI/PauseMenuOptions.cs
+++ FMOD Demo/Assets/Scripts/Generic/UI/PauseMenuOptions.cs
@@ -11,41 +11,65 @@ using UnityEngine.UI;
 public class PauseMenuOptions : MonoBehaviour 
 {
     // Public Vars
-    public Slider m_masterSlider;
-    public Slider m_effectsSlider;
-    public Slider m_dialogueSlider;
-
-    float m_masterLevel, m_effectsLevel, m_dialogueLevel;
+    public Slider m_masterSlider, m_effectsSlider, m_dialogueSlider;
+    public string m_masterBusPath = "Bus:/";
+    public string m_effectsBusPath = "Bus:/SFX";
+    public string m_dialogueBusPath = "Bus:/VO";
 
 	// Private Vars
+    FMOD.Studio.Bus m_masterBus, m_effectsBus, m_dialogueBus;
+    float m_masterLevel, m_effectsLevel, m_dialogueLevel;
+
 
 	void Start () 
 	{
         // Get reference to Master Bus
-        FMODUnity.RuntimeManager.GetBus("");
+        // Master bus can be a blank string and it will get the 
+        m_masterBus = FMODUnity.RuntimeManager.GetBus(m_masterBusPath);
+        m_masterBus.getFaderLevel(out m_masterLevel);
+        m_masterSlider.value = m_masterLevel;
+        m_masterSlider.onValueChanged.AddListener(delegate { OnMasterValueChange(); });
+
         // Get reference to Effects Bus
+        if (m_effectsBusPath != "")
+        {
+            m_effectsBus = FMODUnity.RuntimeManager.GetBus(m_effectsBusPath);
+            m_effectsBus.getFaderLevel(out m_effectsLevel);
+            m_effectsSlider.value = m_effectsLevel;
+            m_effectsSlider.onValueChanged.AddListener(delegate { OnEffectsValueChange(); });
+        }
+        else
+            m_effectsSlider.enabled = false;
 
         // Get reference to Dialogue Bus
+        if (m_dialogueBusPath != "")
+        {
+            m_dialogueBus = FMODUnity.RuntimeManager.GetBus(m_dialogueBusPath);
+            m_dialogueBus.getFaderLevel(out m_dialogueLevel);
+            m_dialogueSlider.value = m_dialogueLevel;
+            m_dialogueSlider.onValueChanged.AddListener(delegate { OnDialogueValueChange(); });
+        }
+        else
+            m_dialogueSlider.enabled = false;
 
-
-        m_masterSlider.onValueChanged.AddListener(delegate { OnMasterValueChange(); });
-        m_effectsSlider.onValueChanged.AddListener(delegate { OnEffectsValueChange(); });
-        m_dialogueSlider.onValueChanged.AddListener(delegate { OnDialogueValueChange(); });
 	}
     
 	#region Private Functions
 
     void OnMasterValueChange()
     {
-
+        m_masterLevel = m_masterSlider.value;
+        m_masterBus.setFaderLevel(m_masterLevel);
     }
     void OnEffectsValueChange()
     {
-
+        m_effectsLevel = m_effectsSlider.value;
+        m_effectsBus.setFaderLevel(m_effectsLevel);
     }
     void OnDialogueValueChange()
     {
-
+        m_dialogueLevel = m_dialogueSlider.value;
+        m_dialogueBus.setFaderLevel(m_dialogueLevel);
     }
 
     #endregion
