d24f18d1a46053b5d8727e824f7cf0cff861ad27
Cameron Baron <info@cameronbaron.com>
2016-09-12 09:50:11 +1000

    Attenuation room: - cannon balls now apply explosion force to objects. - objects have rigidbodys for movement.

--- FMOD Demo/Assets/Scripts/Basic Rooms/Custom Distance Attenuation Room/CannonBall.cs
+++ FMOD Demo/Assets/Scripts/Basic Rooms/Custom Distance Attenuation Room/CannonBall.cs
@@ -1,21 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CannonBall : MonoBehaviour {
+public class CannonBall : MonoBehaviour
+{
+    public float m_explosionRadius = 3.0f;
+    public float m_explosionForce = 3.0f;
 
     ParticleSystem m_particle;
-	// Use this for initialization
-	void Start () {
+
+	void Start ()
+    {
         m_particle = FindObjectOfType<ParticleSystem>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
     void OnCollisionEnter(Collision a_collision)
     {
-        if(a_collision.gameObject.tag == "Ground")
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, m_explosionRadius);
+
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+            if (rb != null)
+            {
+                rb.AddExplosionForce(m_explosionForce, explosionPos, m_explosionRadius, 3.0f);
+            }
+        }
+
+        //if(a_collision.gameObject.tag == "Ground")
         {
             m_particle.transform.position = transform.position;
             m_particle.Play();
