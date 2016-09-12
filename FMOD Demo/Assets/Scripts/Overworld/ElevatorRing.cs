22f2f8b3c3d4274e39026e9f5be8e37b24e7b5be
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-09-12 16:22:22 +1000

    Spinning Elevator ring

---
+++ FMOD Demo/Assets/Scripts/Overworld/ElevatorRing.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorRing : MonoBehaviour
+{
+    public float m_speed;
+    Material m_mat;
+	void Start () {
+        m_mat = GetComponent<Renderer>().materials[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 offset = m_mat.mainTextureOffset;
+        offset.x += m_speed * Time.deltaTime;
+        m_mat.mainTextureOffset = offset;
+    }
+}
