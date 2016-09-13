288a405ea21ebadff56630d82c72e8da18296460
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-09-13 11:22:27 +1000

    Changed car to have gear stick and controllable pedal.

--- FMOD Demo/Assets/Scripts/Basic Rooms/Player Parameter Room/Pedal.cs
+++ FMOD Demo/Assets/Scripts/Basic Rooms/Player Parameter Room/Pedal.cs
@@ -9,18 +9,35 @@ using System.Collections;
 public class Pedal : ActionObject
 {
     public Car m_car;
-    bool m_isAccelerating;
+    float m_acceleration;
 
     void Start () {
-        m_isAccelerating = false;
+        m_acceleration = 1.0f;
 	}
 	void Update () {
 	    
 	}
 
-    public override void ActionPressed(GameObject sender, KeyCode a_key)
+    public override void ActionDown(GameObject sender, KeyCode a_key)
     {
-        m_car.Accelerate();
-        m_isAccelerating = !m_isAccelerating;
+        if (a_key == m_actionKeys[0])
+        {
+            m_acceleration = Mathf.Min(m_acceleration + m_acceleration * Time.deltaTime * 5.0f, 5.0f);
+        }
+        else if(a_key == m_actionKeys[1])
+        {
+            m_acceleration = Mathf.Max(m_acceleration - m_acceleration * Time.deltaTime * 5.0f, 1.0f);
+        }
+        m_car.Accelerate(m_acceleration);
+        Vector3 rot = transform.eulerAngles;
+        rot.x = (m_acceleration / 5.0f) * 40.0f;
+        transform.eulerAngles = rot;
+    }
+    public void Reset()
+    {
+        m_acceleration = 1.0f;
+        Vector3 rot = transform.eulerAngles;
+        rot.x = (m_acceleration / 5.0f) * 40.0f;
+        transform.eulerAngles = rot;
     }
 }
