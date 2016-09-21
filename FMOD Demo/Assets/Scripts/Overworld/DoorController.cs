01d583e3e456568c8503f1bd7d8754ad3f201795
Cameron Baron <twitchin86@gmail.com>
2016-09-21 19:23:11 +1000

    Fixed door script! bloody unity and its scenemanager. Added new script to doors on 1st floor and linked all the tings.

--- FMOD Demo/Assets/Scripts/Overworld/DoorController.cs
+++ FMOD Demo/Assets/Scripts/Overworld/DoorController.cs
@@ -1,50 +1,45 @@
-﻿/*=================================================================
-Project:		#PROJECTNAME#
-Developer:		#DEVOLPERNAME#
-Company:		#COMPANY#
-Date:			#CREATIONDATE#
-==================================================================*/
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(SphereCollider))]
-public class DoorController : ActionObject 
+public class DoorController : ActionObject
 {
-    // Public Vars
-    public GameObject m_door;
-    public string m_sceneToLoad;
+	//---------------------------------Fmod-------------------------------
+	//  Bank reference is the same as and Event, except using BankRef.
+	//  Another difference between Events and Banks is loading and 
+	//  unloading is done using the string and not a class or instance.
+	//--------------------------------------------------------------------
+	[FMODUnity.BankRef]	public string m_bankToLoad;
 
-    //---------------------------------Fmod-------------------------------
-    //  Bank reference is the same as and Event, except using BankRef.
-    //  Another difference between Events and Banks is loading and 
-    //  unloading is done using the string and not a class or instance.
-    //--------------------------------------------------------------------
-    [FMODUnity.BankRef] public string m_bankToLoad;
+	// Public Vars
+	public GameObject m_door;			// Ref to door object that gets moved.
+    public string m_sceneToLoad;		// Name of scene to be loaded/unloaded (needs to be exact).
+	public float m_doorHoldTime;		// Amount of time the door will stay open after leaving the trigger area before closing.
+	public float m_unloadDelay;         // Time until scene is unloaded once door has fully closed.
+	public RoomCompleted m_completeSign;
 
     // Private Vars
-    float m_distToNewPos;
-    bool m_opening = false;
-    Vector3 m_openPos, m_closedPos;
-    SphereCollider m_collider;
+    float m_distToNewPos;				// Distance between door and the current target position.
+    bool m_opening = false;				// Bool used to determine which way the door will move (open/close).
+    Vector3 m_openPos, m_closedPos;     // Positions of door (in local space) when open/closed.
 
-    static AsyncOperation s_async;
+	bool m_completed = false;
+
+	static AsyncOperation s_async;
 
 	void Start ()
     {
-        Application.backgroundLoadingPriority = ThreadPriority.Low;
+        Application.backgroundLoadingPriority = ThreadPriority.Low;		// Setting the thread priority to low forces the async operations to use less cpu.
         m_closedPos = m_door.transform.localPosition;
         m_openPos = m_closedPos + (Vector3.forward * m_door.transform.localScale.z);
-        m_collider = GetComponent<SphereCollider>();
-        m_collider.isTrigger = true;
     }
 	
 	void Update () 
 	{
         m_distToNewPos = Vector3.Distance(m_door.transform.localPosition, (m_opening ? m_openPos : m_closedPos));
         
-        if (m_distToNewPos > 0.3f)
+        if (m_distToNewPos > 0.1f)
         {
             m_door.transform.localPosition -= m_door.transform.right * Time.deltaTime * (m_opening ? 1 : -1);
         }
@@ -52,13 +47,15 @@ public class DoorController : ActionObject
         {
             m_door.transform.localPosition = (m_opening ? m_openPos : m_closedPos);
         }
+
+		if (m_completed)
+			m_completeSign.CompleteRoom();
 	}
 
     public override void ActionPressed(GameObject a_sender, KeyCode a_key)
     {
         //Start loading scene
         StartCoroutine(LoadBankThenScene());
-
     }
 
     #region Private Functions
@@ -66,6 +63,7 @@ public class DoorController : ActionObject
     void OnTriggerEnter(Collider other)
     {
         float dotToCamera = Vector3.Dot(transform.right, (other.transform.position - transform.position).normalized);
+		// If the player enters the trigger from inside the room, automatically open the door.
         if (dotToCamera < 0)
         {
             m_opening = true;
@@ -74,8 +72,8 @@ public class DoorController : ActionObject
 
     void OnTriggerExit(Collider other)
     {
-        StartCoroutine(WaitForDoorToOpenThenClose());
         //Wait for door to close then unload
+        StartCoroutine(WaitForDoorToOpenThenClose());
     }
 
     IEnumerator LoadBankThenScene()
@@ -88,7 +86,6 @@ public class DoorController : ActionObject
             //  sample data.
             //--------------------------------------------------------------------
             FMODUnity.RuntimeManager.LoadBank(m_bankToLoad, true);
-            Debug.Log("Bank loaded");
             //---------------------------------Fmod-------------------------------
             //  Keep yielding the coroutine until the bank has loaded.
             //--------------------------------------------------------------------
@@ -104,11 +101,9 @@ public class DoorController : ActionObject
             {
                 yield return true;
             }
-            Debug.Log("Scene loaded");
         }
 
         m_opening = true;
-        Debug.Log("Load coroutine done");
     }
 
     IEnumerator WaitForDoorToOpenThenClose()
@@ -117,38 +112,31 @@ public class DoorController : ActionObject
         {
             yield return false;
         }
-        Debug.Log("Past while loop");
+		yield return new WaitForSeconds(m_doorHoldTime);
         m_opening = false;
-        //
+
         float dotToCamera = Vector3.Dot(transform.right, (Camera.main.transform.position - transform.position).normalized);
         
         if (dotToCamera > 0)
-        {
-            //dotToCamera = Vector3.Dot(transform.right, (Camera.main.transform.position - transform.position).normalized);
-
-            // Check to see if door is closed
-            Debug.Log(m_distToNewPos);
-            if (m_distToNewPos > 0.3f)
+		{
+			m_completed = true;
+			// Check to see if door is closed
+			while (m_door.transform.localPosition != m_closedPos)
             {
-                Debug.Log("Door not fully closed");
-                //yield return false;
+                yield return false;
             }
+
+			//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+			//Add timer before unloading bank and add lowpass effect to imitate occlusion through closed door//
+			//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+			yield return new WaitForSeconds(m_unloadDelay);
         
-        //    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-        //    //Add timer before unloading bank and add lowpass effect to imitate occlusion through closed door//
-        //    //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
-        //
-        //    // unload bank
-        //    //if (m_bankToLoad != "")
-        //    //
-        //
-        //    // Then unload scene
-        //    Debug.Log("Unload scene");
             if (SceneManager.GetSceneByName(m_sceneToLoad).isLoaded)
             {
+				yield return new WaitForEndOfFrame();
                 SceneManager.UnloadScene(m_sceneToLoad);
-                //FMODUnity.RuntimeManager.UnloadBank(m_bankToLoad);
-                
+				if (m_bankToLoad != "")
+					FMODUnity.RuntimeManager.UnloadBank(m_bankToLoad);                
             }
         }
     }
