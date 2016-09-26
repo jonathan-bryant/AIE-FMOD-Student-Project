3f089720fdf210ba096eb2510d32cf4b6616b265
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-09-26 16:19:58 +1000

    Fixed piano room. not playing notes.

---
+++ FMOD Demo/Assets/DestroyOnCollision.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnCollision : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision a_col)
+    {
+        Destroy(gameObject);
+    }
+}
