4812939bd9cb4958d862f53624c0e4d7630b4d19
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-09-28 10:30:48 +1000

    Piano Room done. Needs more lights plz.

---
+++ FMOD Demo/Assets/KeyRotate.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyRotate : MonoBehaviour
+{
+    bool m_downRotation;
+    Collider m_col;
+
+    void Start()
+    {
+        m_downRotation = false;
+    }
+    void Update()
+    {
+        Vector3 rot = transform.GetChild(0).localEulerAngles;
+        rot.x -= 4 * Time.deltaTime;
+        if (rot.x <= 0)
+        {
+            rot.x = 0;
+        }
+
+        if (!m_col)
+        {
+            m_downRotation = false;
+        }
+        if (m_downRotation)
+        {
+            rot.x += 8 * Time.deltaTime;
+            if (rot.x >= 4)
+            {
+                rot.x = 4;
+            }
+        }
+        transform.GetChild(0).localEulerAngles = rot;
+    }
+
+    void OnTriggerEnter(Collider a_col)
+    {
+        m_col = a_col;
+        m_downRotation = true;
+    }
+    void OnTriggerStay(Collider a_col)
+    {
+        m_downRotation = true;
+    }
+    void OnTriggerExit(Collider a_col)
+    {
+        m_downRotation = false;
+    }
+}
