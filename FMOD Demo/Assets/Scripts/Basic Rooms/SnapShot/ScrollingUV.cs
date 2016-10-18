3816d004bfedc4a1f40de5f17f451d9719beaddf
Cameron Baron <info@cameronbaron.com>
2016-10-18 12:04:36 +1100

    Added Landing pad and flashing arrow to scene. Made arrow flash with scrolling uv script.

---
+++ FMOD Demo/Assets/Scripts/Basic Rooms/SnapShot/ScrollingUV.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingUV : MonoBehaviour
+{
+    public float scrollSpeed = 0.5f;
+    Renderer rend;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rend = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float offset = Time.time * scrollSpeed;
+        rend.materials[0].SetTextureOffset("_MainTex", new Vector2(0, offset));
+
+    }
+}
