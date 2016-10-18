b58b2646eb53dae69a55b8b2669eda9d2711e4ab
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-10-18 16:27:25 +1100

    Added door transceiver. Adjusted studio events.

--- FMOD Demo/Assets/Scripts/Generic/Door.cs
+++ FMOD Demo/Assets/Scripts/Generic/Door.cs
@@ -4,19 +4,12 @@ using UnityEngine.SceneManagement;
 
 public class Door : ActionObject
 {
+    public FMODUnity.StudioEventEmitter m_event;
     public string m_sceneName;
     public float m_angle;
     public float m_originalAngle;
     public float m_duration;
     float m_elapsed;
-    float m_doorTransceiverElapsed;
-    public float DoorElapsed
-    {
-        get
-        {
-            return m_doorTransceiverElapsed / m_duration;
-        }
-    }
     bool m_doorOpen;
     public bool DoorOpen { get { return m_doorOpen; } }
     bool m_openingDoor, m_closingDoor;
@@ -36,32 +29,32 @@ public class Door : ActionObject
         if (m_openingDoor)
         {
             m_elapsed += Time.deltaTime;
-            m_doorTransceiverElapsed += Time.deltaTime;
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, m_originalAngle + (m_angle * (m_elapsed / m_duration)), transform.eulerAngles.z);
             if (m_elapsed >= m_duration)
             {
                 transform.eulerAngles = new Vector3(transform.eulerAngles.x, m_originalAngle + m_angle, transform.eulerAngles.z);
                 m_openingDoor = false;
                 m_doorOpen = true;
+                m_elapsed = m_duration;
                 if (m_sceneName != null && m_sceneName != "")
                 {
                     SceneManager.LoadScene(m_sceneName);
                 }
-                m_doorTransceiverElapsed = m_duration;
+                m_event.SetParameter("Enabled", (m_elapsed / m_duration));
             }
         }
         else if (m_closingDoor)
         {
             m_elapsed += Time.deltaTime;
-            m_doorTransceiverElapsed -= Time.deltaTime;
             transform.eulerAngles = new Vector3(transform.eulerAngles.x, (m_originalAngle + m_angle) - (m_angle * (m_elapsed / m_duration)), transform.eulerAngles.z);
             if (m_elapsed >= m_duration)
             {
                 transform.eulerAngles = new Vector3(transform.eulerAngles.x, m_originalAngle, transform.eulerAngles.z);
                 m_closingDoor = false;
                 m_doorOpen = false;
-                m_doorTransceiverElapsed = 0.0f;
+                m_elapsed = m_duration;
             }
+            m_event.SetParameter("Enabled", 1.0f - (m_elapsed / m_duration));
         }
     }
 
