c4f461c6f9caaf62f44f67561982cc675a3a1aad
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-10-24 16:58:08 +1100

    Fixed speakers. can turn off the speakers now.

---
+++ FMOD Demo/Assets/Scripts/Basic Rooms/Transceiver Room/TransceiverToggle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TransceiverToggle : ActionObject
+{
+    FMODUnity.StudioEventEmitter m_eventEmitter;
+    void Start()
+    {
+        InitGlow();
+        m_eventEmitter = GetComponent<FMODUnity.StudioEventEmitter>();
+    }
+    void Update()
+    {
+        UpdateGlow();
+    }
+    public override void ActionPressed(GameObject sender, KeyCode a_key)
+    {
+        if (m_eventEmitter.IsPlaying())
+        {
+            m_eventEmitter.SetParameter("On", 0.0f);
+            m_eventEmitter.Stop();
+        }
+        else
+        {
+            m_eventEmitter.Play();
+            m_eventEmitter.SetParameter("On", 1.0f);
+        }
+    }
+}
