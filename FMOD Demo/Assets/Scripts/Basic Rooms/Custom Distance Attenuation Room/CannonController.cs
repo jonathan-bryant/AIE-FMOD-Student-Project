6589223a7593fb6239a4c5db4f6d2132e5470454
Matthew Zelenko(Fmod) <matthew@zelenko.com.au>
2016-10-24 16:24:19 +1100

    Fixed cannon room.

--- FMOD Demo/Assets/Scripts/Basic Rooms/Custom Distance Attenuation Room/CannonController.cs
+++ FMOD Demo/Assets/Scripts/Basic Rooms/Custom Distance Attenuation Room/CannonController.cs
@@ -17,12 +17,11 @@ public class CannonController : MonoBehaviour
     |   Create an event for the sound of the cannon changing height that starts up and loops until          |
     |   we set the parameter to tell the event to play the end sound and finish.                            |
     =======================================================================================================*/
-    [FMODUnity.EventRef]    public string m_cannonUpSound;
-    FMOD.Studio.EventInstance m_cannonUpEvent;
-    FMOD.Studio.ParameterInstance m_cannonUpStop;
-    [FMODUnity.EventRef]    public string m_cannonDownSound;
-    FMOD.Studio.EventInstance m_cannonDownEvent;
-    FMOD.Studio.ParameterInstance m_cannonDownStop;
+    [FMODUnity.EventRef]
+    public string m_cannonSoundPath;
+    FMOD.Studio.EventInstance m_cannonEvent;
+    FMOD.Studio.ParameterInstance m_cannonStopParameter;
+    FMOD.Studio.ParameterInstance m_cannonDirectionParameter;
     FMOD.Studio.PLAYBACK_STATE m_playState;
 
     [FMODUnity.EventRef]
@@ -35,25 +34,23 @@ public class CannonController : MonoBehaviour
     float m_power;
     float m_selectedAngle;
     bool m_isActive;
-
-    public float m_timer;
-    float m_elapsed;
+    
+    public float m_fireDelay = 0.5f;
+    float m_elapsedFireDelay;
+    float m_cannonChangeSpeed = 10.0f;
 
     void Start()
     {
         /*===============================================Fmod====================================================
         |   Create the events, set the parameter for exiting the loop and set the 3D attributes.                |
         =======================================================================================================*/
-        m_cannonUpEvent = FMODUnity.RuntimeManager.CreateInstance(m_cannonUpSound);
-        m_cannonUpEvent.getParameter("Stop Point", out m_cannonUpStop);
-        m_cannonUpEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
-        m_cannonDownEvent = FMODUnity.RuntimeManager.CreateInstance(m_cannonDownSound);
-        m_cannonDownEvent.getParameter("Stop Point", out m_cannonDownStop);
-        m_cannonDownEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
+        m_cannonEvent = FMODUnity.RuntimeManager.CreateInstance(m_cannonSoundPath);
+        m_cannonEvent.getParameter("Stop Point", out m_cannonStopParameter);
+        m_cannonEvent.getParameter("Direction", out m_cannonDirectionParameter);
+        m_cannonEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
         m_cannonFireEvent = FMODUnity.RuntimeManager.CreateInstance(m_cannonFireSound);
         m_cannonFireEvent.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(transform.position));
-
-        m_elapsed = 0.0f;
+        
         m_currentAngle = 30.0f;
         m_selectedAngle = 30.0f;
         m_power = 10.0f;
@@ -61,7 +58,6 @@ public class CannonController : MonoBehaviour
     }
     void FixedUpdate()
     {
-        m_elapsed += Time.fixedDeltaTime;
         if (m_isActive)
         {
             if (m_currentAngle != m_selectedAngle)
@@ -71,14 +67,14 @@ public class CannonController : MonoBehaviour
                     /*===============================================Fmod====================================================
                     |   Check to see if the event is already playing, if not then start the event from the beginning.       |
                     =======================================================================================================*/
-                    m_cannonUpEvent.getPlaybackState(out m_playState);
+                    m_cannonEvent.getPlaybackState(out m_playState);
                     if (m_playState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
                     {
-                        m_cannonUpStop.setValue(0.0f);
-                        m_cannonUpEvent.start();
+                        m_cannonStopParameter.setValue(0.0f);
+                        m_cannonEvent.start();
                     }
 
-                    m_currentAngle -=  5.0f * Time.fixedDeltaTime;
+                    m_currentAngle -= m_cannonChangeSpeed * Time.fixedDeltaTime;
                     if (m_currentAngle < m_selectedAngle)
                     {
                         m_currentAngle = m_selectedAngle;
@@ -92,15 +88,15 @@ public class CannonController : MonoBehaviour
                     /*===============================================Fmod====================================================
                     |   Check to see if the event is already playing, if not then start the event from the beginning.       |
                     =======================================================================================================*/
-                    m_cannonDownEvent.getPlaybackState(out m_playState);
+                    m_cannonEvent.getPlaybackState(out m_playState);
                     if (m_playState != FMOD.Studio.PLAYBACK_STATE.PLAYING)
                     {
-                        m_cannonDownStop.setValue(0.0f);
-                        m_cannonDownEvent.start();
+                        m_cannonStopParameter.setValue(0.0f);
+                        m_cannonEvent.start();
                     }
 
-                    m_currentAngle += 5.0f * Time.fixedDeltaTime;
-                    if(m_currentAngle > m_selectedAngle)
+                    m_currentAngle += m_cannonChangeSpeed * Time.fixedDeltaTime;
+                    if (m_currentAngle > m_selectedAngle)
                     {
                         m_currentAngle = m_selectedAngle;
                     }
@@ -111,17 +107,20 @@ public class CannonController : MonoBehaviour
             }
             else
             {
-                m_cannonFireEvent.start();
-                /*===============================================Fmod====================================================
-                |               Now we went to exit the event loop and play the end of the sound.                       |
-                =======================================================================================================*/
-                m_cannonDownStop.setValue(1.0f);
-                m_cannonUpStop.setValue(1.0f);
-                m_elapsed = 0.0f;
-                m_isActive = false;
-                GameObject ball = Instantiate(m_cannonBall, m_cannon.transform.GetChild(0).position - (m_cannon.transform.GetChild(0).up), Quaternion.identity) as GameObject;
-                ball.transform.SetParent(transform);
-                ball.GetComponent<Rigidbody>().AddForce(-m_cannon.transform.GetChild(0).up * m_power, ForceMode.Impulse);
+                m_cannonStopParameter.setValue(1.0f);
+                m_elapsedFireDelay += Time.fixedDeltaTime;
+                if (m_elapsedFireDelay >= m_fireDelay)
+                {
+                    m_elapsedFireDelay = 0.0f;
+                    m_cannonFireEvent.start();
+                    /*===============================================Fmod====================================================
+                    |               Now we went to exit the event loop and play the end of the sound.                       |
+                    =======================================================================================================*/
+                    m_isActive = false;
+                    GameObject ball = Instantiate(m_cannonBall, m_cannon.transform.GetChild(0).position - (m_cannon.transform.GetChild(0).up), Quaternion.identity) as GameObject;
+                    ball.transform.SetParent(transform);
+                    ball.GetComponent<Rigidbody>().AddForce(-m_cannon.transform.GetChild(0).up * m_power, ForceMode.Impulse);
+                }
             }
         }
     }
@@ -130,20 +129,17 @@ public class CannonController : MonoBehaviour
     {
         if (m_isActive)
             return;
-        if (m_elapsed < m_timer)
-            return;
-        m_elapsed = 0.0f;
 
         switch (a_index)
         {
             case 1:
                 m_selectedAngle = 30.0f;
-                m_power = 15.0f;
+                m_power = 20.0f;
                 m_isActive = true;
                 break;
             case 2:
                 m_selectedAngle = 45.0f;
-                m_power = 20.0f;
+                m_power = 15.0f;
                 m_isActive = true;
                 break;
             case 3:
@@ -153,6 +149,14 @@ public class CannonController : MonoBehaviour
                 break;
             default:
                 break;
+        }
+        if (m_selectedAngle > m_currentAngle)
+        {
+            m_cannonStopParameter.setValue(1.0f);
+        }
+        else
+        {
+            m_cannonStopParameter.setValue(-1.0f);
         }
     }
 }
